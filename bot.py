3834bf6554dac9e50e72d789b7c55a5976c7bdbc
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-11-08 23:58:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def start(update, context):
     update.message.reply_text(
         f"<b>Hi {update.message.from_user.first_name}!<\b>\n\n"
         "I'm Golinksrt bot. Just send me link and get short link\n\n/help for more details\n\n"
-        "<b>Join my  <a href="https://t.me/Golinksrt/"update channel</a><\b>"
+        "<b>Join my  <a href=https://t.me/Golinksrt/update channel</a><\b>"
 
 
 def help_command(update, context):
